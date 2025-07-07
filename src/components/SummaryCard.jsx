57ca841aaf5567446d096c383b4b8f5c57bc9c09
elishaherrera <elisha.gh96@gmail.com>
2025-07-07 15:37:27 -0400

    Enhance Storybook setup and components
    - Updated preview.js to ensure proper CSS import.
    - Added shadow effect to ProjectCard and SummaryCard components for improved UI.
    - Introduced PropTypes validation in SummaryCard for better type checking.
    - Created stories for ProjectCard and SummaryCard to facilitate testing in Storybook.

--- src/components/SummaryCard.jsx
+++ src/components/SummaryCard.jsx
@@ -1,3 +1,4 @@
+import PropTypes from "prop-types";
 const colorStatus = {
     "green": "text-green-500",
     "red": "text-red-500",
@@ -6,7 +7,7 @@ const colorStatus = {
 export function SummaryCard({ numero, titulo, subtitulo, color }) {
     return (
 
-        <div className=" px-8 py-8 w-68 h-35 rounded-2xl m-10 p-4 justify-items-start mx-2">
+        <div className=" px-8 py-8 w-68 h-35 rounded-2xl m-10 p-4 justify-items-start mx-2 shadow-lg">
             <h1 className="text-3xl font-bold">{numero}</h1>
             <h2 className="text-gray-500 pt-1">{titulo}</h2>
             <h3 className={`text-xs pt-2 ${colorStatus[color]}`}>{subtitulo}</h3>
@@ -15,3 +16,9 @@ export function SummaryCard({ numero, titulo, subtitulo, color }) {
     );
 }
 export default SummaryCard;
+SummaryCard.propTypes = {
+    numero: PropTypes.string.isRequired,
+    titulo: PropTypes.string.isRequired,
+    subtitulo: PropTypes.string.isRequired,
+    color: PropTypes.oneOf(["green", "red"]).isRequired,
+};
