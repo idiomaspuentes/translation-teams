4c2cbd92c4f7bdf8afaffc1531797500c01e1af7
elishaherrera <elisha.gh96@gmail.com>
2025-07-18 16:00:47 -0400

    Add Tabla, EncabezadoTabla, and FilaTabla components with Storybook stories

---
+++ src/stories/FilaTabla.stories.js
@@ -0,0 +1,19 @@
+
+import FilaTabla from "../components/FilaTabla"
+export default {
+    title: "FilaTabla",
+    component: FilaTabla,
+
+}
+
+export const Ejemplo = {
+    args: {
+        nombre: "María Fátima",
+        cantidadDeProyectos: 8,
+        palabrasPorMes: "15,240",
+        promedioCalidad: "98.2%",
+        diasPromedio: "3.8 dias",
+        estado: "Activo",
+
+    },
+};
