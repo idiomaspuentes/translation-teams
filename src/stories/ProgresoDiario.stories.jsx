f7ac4a687791fb27bc537210a771988b5e69870c
elishaherrera <elisha.gh96@gmail.com>
2025-09-24 16:35:11 -0400

    Add ProgresoDiario component and its story for daily progress visualization

---
+++ src/stories/ProgresoDiario.stories.jsx
@@ -0,0 +1,10 @@
+import ProgresoDiario from "../components/ProgresoDiario";
+
+export default {
+  title: "Progreso Diario",
+  component: ProgresoDiario,
+};
+
+export const Ejemplo = {
+  args: {},
+};
