6c62c463719cbda4043fff8fec7492b4c068a2c9
elishaherrera <elisha.gh96@gmail.com>
2025-11-07 15:48:03 -0400

    Refactor TaskColumn component to accept color prop and update Kanban page with color-coded columns

--- src/components/TaskColumn.jsx
+++ src/components/TaskColumn.jsx
@@ -1,6 +1,20 @@
 import TaskCard from "./TaskCard.jsx";
 
-export default function TaskColumn({ children, titulo, numero, agregar }) {
+const colors = {
+  gray: "bg-gray-500",
+  orange: "bg-yellow-500",
+  blue: "bg-blue-500",
+  violet: "bg-violet-500",
+  green: "bg-green-500",
+};
+
+export default function TaskColumn({
+  children,
+  titulo,
+  numero,
+  agregar,
+  color,
+}) {
   return (
     <div className="flex-1 pt-[1rem]  pb-[2rem] pl-[2rem] overflow-x-auto overflow-y-hidden">
       <div className="flex gap-6 h-full min-w-max ">
@@ -9,20 +23,22 @@ export default function TaskColumn({ children, titulo, numero, agregar }) {
           {/* columna - encabezado */}
           <div className="px-4 py-4  rounded-[0.75rem_0.75rem_0_0] border-solid border-b-1 border-[#e2e8f0] flex justify-between items-center">
             <div className="font-semibold flex items-center gap-[0.5rem]">
-              <div className={`w-2 h-2 bg-[#6b7280] rounded-[50%]`}></div>
+              <div className={`w-2 h-2 ${colors[color]} rounded-[50%]`}></div>
               {titulo}
             </div>
             <span className="bg-[#e2e8f0] text-[#64748b] p-[0.25rem_0.5rem] rounded-[1rem] text-xs font-medium">
               {numero}
             </span>
           </div>
-          <div className="flex-1 p-4 flex flex-col gap-4 overflow-y-auto">
+          <div className="flex-1 flex flex-col gap-4 overflow-y-auto">
             {/* Tarjeta - tarea */}
-            {children}
+            <div className="mt-2 space-y-6 overflow-y-auto h-70 pr-4 p-4">
+              {children}
+            </div>
             {/* Boton - agregar */}
             <a
               href="#"
-              className="flex items-center justify-center gap-1 p-3 bg-white border-2 border-dashed border-[#d1d5db] rounded-lg text-[#64748b] no-underline transition-[all_0.2s] text-sm hover:border-[#3b82f6] hover:text-[#3b82f6] hover:bg-[#f8fafc]"
+              className="flex items-center justify-center gap-1 p-3 m-4 bg-white border-2 border-dashed border-[#d1d5db] rounded-lg text-[#64748b] no-underline transition-[all_0.2s] text-sm hover:border-[#3b82f6] hover:text-[#3b82f6] hover:bg-[#f8fafc]"
             >
               <span>+</span>
               {agregar}
