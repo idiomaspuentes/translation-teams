4eea7c98ac0711c2f34c2dfd1a2ad701dac1f132
elishaherrera <elisha.gh96@gmail.com>
2025-12-05 17:45:04 -0400

    Refactor ListaDeTareasDelProyecto component structure and update story examples for improved clarity and organization

--- src/components/ListaDeTareasDelProyecto.jsx
+++ src/components/ListaDeTareasDelProyecto.jsx
@@ -6,38 +6,40 @@ export default function ListaDeTareasDelProyecto({ fases }) {
   return (
     <>
       {fases.map((fase, index) => (
-        <TareaProyecto
-          titulo={fase.titulo}
-          descripcion={fase.descripcion}
-          numTareas={fase.numTareas}
-          numSubtareas={fase.numSubtareas}
-          porcentaje={fase.porcentaje}
-        >
-          {fase.tareas &&
-            fase.tareas.map((tarea, idx) => (
-              <>
-                <SubtareaProyecto
-                  listo={tarea.listo}
-                  titulo={tarea.titulo}
-                  subtitulo={tarea.subtitulo}
-                  subtitulo2={tarea.subtitulo2}
-                  iniciales={tarea.iniciales}
-                  asignado={tarea.asignado}
-                  prioridad={tarea.prioridad}
-                  estado={tarea.estado}
-                  diaLimite={tarea.diaLimite}
-                />
-                {tarea.subtareas &&
-                  tarea.subtareas.map((subtarea, i) => (
-                    <ChecklistDeSubtareaDeProyecto
-                      listo={subtarea.listo}
-                      titulo={subtarea.titulo}
-                      asignado={subtarea.asignado}
-                    />
-                  ))}
-              </>
-            ))}
-        </TareaProyecto>
+        <div>
+          <TareaProyecto
+            titulo={fase.titulo}
+            descripcion={fase.descripcion}
+            numTareas={fase.numTareas}
+            numSubtareas={fase.numSubtareas}
+            porcentaje={fase.porcentaje}
+          >
+            {fase.tareas &&
+              fase.tareas.map((tarea, idx) => (
+                <>
+                  <SubtareaProyecto
+                    listo={tarea.listo}
+                    titulo={tarea.titulo}
+                    subtitulo={tarea.subtitulo}
+                    subtitulo2={tarea.subtitulo2}
+                    iniciales={tarea.iniciales}
+                    asignado={tarea.asignado}
+                    prioridad={tarea.prioridad}
+                    estado={tarea.estado}
+                    diaLimite={tarea.diaLimite}
+                  />
+                  {tarea.subtareas &&
+                    tarea.subtareas.map((subtarea, i) => (
+                      <ChecklistDeSubtareaDeProyecto
+                        listo={subtarea.listo}
+                        titulo={subtarea.titulo}
+                        asignado={subtarea.asignado}
+                      />
+                    ))}
+                </>
+              ))}
+          </TareaProyecto>
+        </div>
       ))}
     </>
   );
