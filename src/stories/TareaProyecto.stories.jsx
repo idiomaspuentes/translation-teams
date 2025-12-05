0d3c72fb846ace32d0715dd74d95fb0793cdbf3d
elishaherrera <elisha.gh96@gmail.com>
2025-12-05 15:14:27 -0400

    Add TareaProyecto component with progress bar and story example

---
+++ src/stories/TareaProyecto.stories.jsx
@@ -0,0 +1,16 @@
+import TareaProyecto from "../components/TareaProyecto";
+
+export default {
+  title: "Tarea proyecto",
+  component: TareaProyecto,
+};
+
+export const Ejemplo = {
+  args: {
+    titulo: "Traducción",
+    descripcion: "Contenido principal del manual",
+    numTareas: 6,
+    numSubtareas: 18,
+    porcentaje: 75,
+  },
+};
