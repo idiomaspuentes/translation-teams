7b1667c6c6e319323951f49e3bff305285695eba
elishaherrera <elisha.gh96@gmail.com>
2025-12-30 19:13:20 -0400

    Add MenuPrincipalProyecto component and its Storybook story

---
+++ src/components/MenuPrincipalProyecto.jsx
@@ -0,0 +1,24 @@
+export default function MenuPrincipalProyecto({}) {
+  return (
+    <>
+      <div>
+        <div className="flex justify-between">
+          <div>
+            <h1 className="text-3xl font-bold">Proyectos</h1>
+          </div>
+          <div className="space-x-4">
+            <button className="px-6 py-3 text-sm border border-gray-300 rounded-lg cursor-pointer">
+              Importar
+            </button>
+            <button className="px-6 py-3 text-sm border border-gray-300 rounded-lg cursor-pointer">
+              Exportar
+            </button>
+            <button className="px-6 py-3 text-sm border border-gray-300 bg-blue-500 text-white rounded-lg cursor-pointer">
+              + Nuevo proyecto
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
