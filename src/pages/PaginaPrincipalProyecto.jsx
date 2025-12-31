fd1aafb44c770da311ff3f7e1415a92f796c016d
elishaherrera <elisha.gh96@gmail.com>
2025-12-31 08:59:01 -0400

    Refactor IntegracionDeLaPaginaPrincipalDeProyectos layout to organize project components into two columns

--- src/pages/PaginaPrincipalProyecto.jsx
+++ src/pages/PaginaPrincipalProyecto.jsx
@@ -1,6 +1,9 @@
 import MenuPrincipalProyecto from "../components/MenuPrincipalProyecto";
 import BuscarProyectos from "../components/BuscarProyectos";
 import ProyectoActivo from "../components/ProyectoActivo";
+import ProyectoRevision from "../components/ProyectoRevision";
+import ProyectoCompletado from "../components/ProyectoCompletado";
+import ProyectoTarjeta from "../components/ProyectoTarjeta";
 
 function IntegracionDeLaPaginaPrincipalDeProyectos() {
   return (
@@ -11,8 +14,15 @@ function IntegracionDeLaPaginaPrincipalDeProyectos() {
       <div className="m-8">
         <BuscarProyectos />
       </div>
-      <div className="m-8">
-        <ProyectoActivo />
+      <div className="flex ">
+        <div className="m-8 flex flex-col gap-12">
+          <ProyectoActivo />
+          <ProyectoCompletado />
+        </div>
+        <div className="m-8 flex flex-col gap-12">
+          <ProyectoRevision />
+          <ProyectoTarjeta />
+        </div>
       </div>
     </>
   );
